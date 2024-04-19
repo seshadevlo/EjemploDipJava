a84aee2d4b0c75c6df4cc517b71ac8cf0e062126
seshadevlo <l.vet.operaciones@gmail.com>
2024-04-18 23:22:26 -0400

    se añade comentario sobre la interfaz

--- src/ejemplos_DIP_Aplica/INotifierService.java
+++ src/ejemplos_DIP_Aplica/INotifierService.java
@@ -1,5 +1,8 @@
 package ejemplos_DIP_Aplica;
 
+
+/* una interfaz nos permite poder crear una variable de esta misma,
+ *  para luego asignarle las instancias de clases que la implementan.*/
 public interface INotifierService {
 	void enviarMensaje(String mensaje);
 }
